49e1c17f1a455663e8611ad709364db44144f154
Samwise2310 <samwise2310@gmail.com>
2020-07-10 04:21:46 -0500

    Create cannonBallFire.cs
    Uploading cannonBallFire.cs to master

---
+++ cannonBallFire.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonBallFire : MonoBehaviour
+{
+
+
+
+    //Declaring Variables
+        // This will let me pass the cannonBall object into the scene
+    public GameObject cannonBall;
+        // Speed of the cannonBall
+    public float Speed = 20f;
+        // Speed1 is the Random speed that the ball is launched at
+    public float Speed1 = 0f;
+        // Right controls the left/right angle that the ball is launched at
+    public float Right = 0f;
+        // Vertical
+    public float Vertical = 20f;
+        // Here I can pass in the Rigidbody of the cannonBall
+    public Rigidbody rb;
+        // This will be the target position
+    public Vector3 targetPos;
+        // Starting Position will hold the start position if I need it
+    public Vector3 startPos;
+        // This will hold the Random Value that will throw objects.
+    public float rand = 3f;
+
+        // Damage will hold the amount of damage this will deal if it hits the player
+    public float damage = 50f;
+
+        // This Rigidbody will hold the rigidbody of the each clone
+    public Rigidbody tempRigidbody;
+
+        
+
+    
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Invoke("fire", rand);
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Fire is called every (random)
+    void fire()
+    {
+
+
+        GameObject clone = Instantiate(cannonBall);
+
+        Destroy(clone.gameObject, 5f);
+
+
+        tempRigidbody = clone.GetComponent<Rigidbody>();
+
+
+
+
+            // This Invoke is invoking the fire function every rand time
+        Invoke("fire", rand);
+
+
+            // This is randomizing the time between shots
+        rand = Random.Range(1f, 10f);
+        print("Rand = " + rand);
+            // This is Randomizing the foward speed
+        Speed1 = Random.Range(120f, 250f);
+        print("Speed1 = " + Speed1);
+            // This is Randomizing the Vertical velocity
+        Vertical = Random.Range(200f, 225f);
+        print("Vertacle = " + Vertical);
+            // This is Randomizing the left/right "angle"
+        Right = Random.Range(-20f, 20f);
+        print("Right Random = " + Right);
+
+
+            // Adding force to the RigidBody of the object
+        tempRigidbody.AddForce(transform.forward * Speed1);    // Foward
+        tempRigidbody.AddForce(transform.up * Vertical);       // Up
+        tempRigidbody.AddForce(transform.right * Right);       // Left/Right
+
+
+    }
+      
+
+}
