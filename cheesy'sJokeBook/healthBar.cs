105fcff1366fb8c30668d726dc5a0bcf25495d02
Samwise2310 <samwise2310@gmail.com>
2020-07-17 17:27:47 -0500

    Updated HealthBar.cs
    Update HealthBar.cs

--- cheesy'sJokeBook/healthBar.cs
+++ cheesy'sJokeBook/healthBar.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+
+    // Declaring Variables
+        // Slider will hold the slider passed through in Unity
+    public Slider slider;
+
+
+
+
+    public void SetMaxHealth(int health)
     {
-        
+
+        slider.maxValue = health;
+        slider.value = health;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetHealth(int health)
     {
-        
+
+        slider.value = health;
+
     }
+
+
 }
